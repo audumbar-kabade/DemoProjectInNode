8eb8c3cfac2adb8165080d042173668a32af7dc4
Shruti Shiradwade <Shrushiradwade@gmail.com>
2021-09-13 20:06:35 +0530

    morgan commented

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var app = express();
 let userRouter= require('./routes/users.routes');
 let deptRouter= require('./routes/dept.routes');
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
